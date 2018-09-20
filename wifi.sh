6c67a7bbb16d65e2017d257877ebbf429ecbd280
Florian Siepe <flo.siepe@gmail.com>
2018-09-20 08:31:00 +0200

    Fixed wifi script

--- wifi.sh
+++ wifi.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 HOST=google.de
 
 if ping -c1 $HOST 1>/dev/null 2>/dev/null
 then
 	echo "We have a internet connection"
+	env > success.list
 else
 	echo "No internet connection!"
 	echo "Running lynx script"
-	lynx --cmd_script=staff.log --accept_all_cookies http://www.google.de
+	env > fail.list
+	lynx --cmd_script=RPIAlarm/staff.log --accept_all_cookies http://www.google.de
 fi
