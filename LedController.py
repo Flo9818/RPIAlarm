a0abfb5e0bcb1a7e480b3f4daaf805fa01bfae4a
Florian Siepe <flo.siepe@gmail.com>
2018-09-23 15:27:20 +0200

    Connected rpi client and led controller

--- LedController.py
+++ LedController.py
@@ -1,7 +1,7 @@
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
-from luma.core.legacy import text, show_message
+from luma.core.legacy import text
 from luma.core.legacy.font import proportional, CP437_FONT
 from luma.core.virtual import viewport
 from time import sleep
@@ -9,24 +9,26 @@ from time import sleep
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=2, block_orientation=90, rotate=2)
 virtual = viewport(device, width=200, height=20)
-def draw():
-    with canvas(virtual) as draw:
-        #draw.rectangle(device, outline="white")
-	text(draw, (16,0), "On Air", fill="white", font=proportional(CP437_FONT))
-    offset=0
-    while True:
-	virtual.set_position((offset, 0))
-	sleep(0.1)
-	offset=offset+1
-	if offset == 60:
-		offset=0
 
-def showText():
-    show_message(device, "On Air", font=proportional(CP437_FONT))
 
-try:
-	draw()
-except KeyboardInterrupt:
-	pass
-finally:
-	device.clear()
+class LedController:
+    def __init__(self):
+        self.enabled = False
+
+    def draw(self):
+        with canvas(virtual) as draw:
+            text(draw, (16, 0), "On Air", fill="white",
+                 font=proportional(CP437_FONT))
+        offset = 0
+        while self.enabled:
+            virtual.set_position((offset, 0))
+            sleep(0.1)
+            offset = offset+1
+            if offset == 60:
+                offset = 0
+
+    def setEnabled(self, enabled):
+        self.enabled = enabled
+
+
+device.clear()
--- WebSocketClientRpi.py
+++ WebSocketClientRpi.py
@@ -1,32 +1,42 @@
 import websocket
 import ssl
 import json
+from LedController import LedController
 
-destUri = "ws://vileadgentest.westeurope.cloudapp.azure.com:3000";
+lc = LedController()
+
+destUri = "ws://vileadgentest.westeurope.cloudapp.azure.com:3000"
+
 
 def on_message(ws, message):
     try:
         msg = json.loads(message.split('- ')[1])
         if msg['COMMAND'] == 'enable':
             print('Enable')
+            lc.setEnabled(True)
         elif msg['COMMAND'] == 'disable':
             print('Disable')
+            lc.setEnabled(False)
     except:
         pass
+
 
 def on_error(ws, error):
     print(error)
 
+
 def on_close(ws):
     print("### closed ###")
+
 
 def on_open(ws):
     print("### opened ###")
 
+
 ws = websocket.WebSocketApp(destUri,
-    on_message = on_message,
-    on_error = on_error,
-    on_close = on_close)
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
 ws.on_open = on_open
 #ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 ws.run_forever()
